1867941bbaab006a55421c00d4e19272a176bd85
Luis Porto <luis.porto@dafiti.com.br>
2023-03-03 21:13:32 -0300

    envio inicial

---
+++ src/main/java/com/luisow748/springsecurityimpl/SpringSecurityImplApplication.java
@@ -0,0 +1,13 @@
+package com.luisow748.springsecurityimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisow748/springsecurityimpl/SpringSecurityImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisow748.springsecurityimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
